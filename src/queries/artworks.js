904984a07d0410c73f42c04bc808c506af1e9b5f
Adam Soltys <asoltys@gmail.com>
2022-03-18 15:41:42 -0700

    order by asc edition number

--- src/queries/artworks.js
+++ src/queries/artworks.js
@@ -214,7 +214,7 @@ export const getArtworksByUsername = (username) => `query {
 }`;
 
 export const getArtworksByTag = (tag) => `query {
-  artworks(where: {tags: {tag: {_eq: "${tag}"}}}, order_by: { created_at: asc }) {
+  artworks(where: {tags: {tag: {_eq: "${tag}"}}}, order_by: { edition: asc }) {
     ${fields}
   }
 }`;
