9c7c2ec5dbf01a15bb58b71c8f5340bb7667b75f
Ash Coca <ashvincoca@gmail.com>
2022-03-18 17:34:16 -0700

    Manually reverting autoplay changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -46,7 +46,10 @@
 <div class="flex secondary-header mt-20 mb-20 text-white">
   <video
     class="lazy cover absolute secondary-header"
+    autoplay
     muted
+    playsinline
+    loop
   >
     <source src="/bob.mp4" />
     Your browser does not support HTML5 video.
