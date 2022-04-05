18a8a54976166a1bf0dca0c7b2df54f10ecde507
Adam Soltys <asoltys@gmail.com>
2022-04-05 16:05:06 -0700

    add promo code field to registration form

--- src/lib/register.js
+++ src/lib/register.js
@@ -4,7 +4,7 @@ import { createWallet } from "$lib/wallet";
 import wordlist from "$lib/wordlist";
 import { generateMnemonic } from "bip39";
 
-export const register = async (email, username, password) => {
+export const register = async (email, username, password, promo) => {
   if (!validateEmail(email)) throw new Error("Invalid email");
   if (password.length < 8) throw new Error("Password must be 8 characters");
 
@@ -13,6 +13,7 @@ export const register = async (email, username, password) => {
     .post({
       email,
       password,
+      promo,
       username,
       ...createWallet(
         generateMnemonic(undefined, undefined, wordlist),
